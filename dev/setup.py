5c2e3fdf2c2cfd863a905f6d80faac7735051781
yewang726 <ye.wang@anu.edu.au>
2019-06-02 03:15:39 +1000

    setup script

---
+++ dev/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Solstice-py',
+      version='0.0',
+      description='Scripts and wrappers for Solstice ray-tracing software',
+      author='Ye Wang',
+      author_email='ye.wang@anu.edu.au',
+      url='https://github.com/anustg/solstice-scripts',
+      packages=['srcPy', 'srcPy.layout'],
+      license="GPL v3.0 or later, see LICENSE file"
+     )
+
