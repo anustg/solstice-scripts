dfee7b386ff520d73590a3f03307084e46b77ef7
yewang726 <ye.wang@anu.edu.au>
2020-07-28 08:46:08 +1000

    update version name, for revising readme.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(name='solsticepy'
-	,version='0.3'
+	,version='0.3.1'
 	,author='Ye Wang'
 	,author_email='ye.wang@anu.edu.au'
 	,description="Scripts and wrappers for Solstice ray-tracing software"
